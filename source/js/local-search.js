d89fe2d6f804db79ef119eb19cb1fd9ac4ba90c4
voidking <voidking@qq.com>
2021-08-25 07:51:16 +0800

    modify: localsearch没有配置cdn字段也不会报错

--- source/js/local-search.js
+++ source/js/local-search.js
@@ -14,7 +14,7 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   let path = CONFIG.root + searchPath;
   // Use jsDelivr CDN to accelerate the speed of loading search.xml
-  if (CONFIG.localsearch.cdn.enable === true && CONFIG.localsearch.cdn.url !== null){
+  if ('cdn' in CONFIG.localsearch && CONFIG.localsearch.cdn.enable === true && CONFIG.localsearch.cdn.url !== null){
     path = CONFIG.localsearch.cdn.url;
   }
 
