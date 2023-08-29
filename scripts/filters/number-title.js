91a9116d25c2e8a4fda4fd8dd9644fd6d624f3b6
voidking <voidking@qq.com>
2023-08-29 15:29:35 +0800

    bugfix: 缺少一级二级标题时，文章标题分级自动编号后多出点，例如 ".1. 前言" 和 ".2.1. 思路"

--- scripts/filters/number-title.js
+++ scripts/filters/number-title.js
@@ -1,91 +1,48 @@
-'use strict'
+'use strict';
 
-let cheerio
+let cheerio;
 
 hexo.extend.filter.register(
   'after_post_render',
   (data) => {
-    console.log('number-title')
-    if (!cheerio) cheerio = require('cheerio')
+    console.log('number-title');
+    if (!cheerio) cheerio = require('cheerio');
 
     const $ = cheerio.load(data.content, {
       decodeEntities: false,
-    })
+    });
     const $excerpt = cheerio.load(data.excerpt, {
       decodeEntities: false,
-    })
+    });
 
-    let elHlist = $('h1,h2,h3,h4,h5,h6')
-    // h1到h6的标题编号
-    let h1 = '',
-      h2 = '',
-      h3 = '',
-      h4 = '',
-      h5 = '',
-      h6 = '',
-      // 相对于父级的第几个子标题
-      h1index = 1,
-      h2index = 1,
-      h3index = 1,
-      h4index = 1,
-      h5index = 1,
-      h6index = 1
+    let elHlist = $('h1, h2, h3, h4, h5, h6');
+    // Initialize parent indexes
+    let parentIndexes = [0, 0, 0, 0, 0, 0];
 
     elHlist.each((i, el) => {
-      // 标题编号
-      let hindexString = ''
-      switch ($(el).get(0).tagName) {
-        case 'h1': {
-          h2index = 1
-          h1 = h1index
-          h1index++
-          hindexString = h1
-          break
-        }
-        case 'h2': {
-          h3index = 1
-          h2 = h1 + '.' + h2index
-          h2index++
-          hindexString = h2
-          break
-        }
-        case 'h3': {
-          h4index = 1
-          h3 = h2 + '.' + h3index
-          h3index++
-          hindexString = h3
-          break
-        }
-        case 'h4': {
-          h5index = 1
-          h4 = h3 + '.' + h4index
-          h4index++
-          hindexString = h4
-          break
-        }
-        case 'h5': {
-          h6index = 1
-          h5 = h4 + '.' + h5index
-          h5index++
-          hindexString = h5
-          break
-        }
-        case 'h6': {
-          h6 = h5 + '.' + h6index
-          h6index++
-          hindexString = h6
-          break
-        }
+      const tagName = $(el).get(0).tagName.toLowerCase().trim();
+      const level = parseInt(tagName[1]); // Extract the heading level from the tag name (h1, h2, etc.)
+
+      // Update parent index for current level
+      parentIndexes[level - 1]++;
+
+      // Reset parent indexes for lower levels
+      for (let j = level; j < parentIndexes.length; j++) {
+        parentIndexes[j] = 0;
       }
+
+      // Create the index string without leading zero and dot
+      const indexString = parentIndexes.slice(0, level).filter(index => index !== 0).join('.') + '.';
+
       $(el).prepend(
         $('<span />')
           .addClass('post-title-index')
-          .text(hindexString + '. ')
-      )
-    })
+          .text(indexString === '.' ? '' : indexString + ' ')
+      );
+    });
 
-    data.content = $.html()
-    data.excerpt = $excerpt.html()
+    data.content = $.html();
+    data.excerpt = $excerpt.html();
   },
   10
-)
+);
